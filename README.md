304fcb97370b2fd08a0007b4b7509531129104fb
Shreya <Shreyaroxx777@gmail.com>
2020-06-14 18:52:10 +0530

    updated

--- README.md
+++
@@ -1,3 +0,0 @@
-## License 📝
-
-MIT.
--- cypress/e2e/a11y.test.js
+++
@@ -1,5 +0,0 @@
-describe('Accessibility checks', () => {
-  it('Main Page', () => cy.visit('/').injectAxe().checkA11y());
-
-  it('404 Page', () => cy.visit('/404').injectAxe().checkA11y());
-});
