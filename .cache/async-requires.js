89f78acc133e5bc737b317e4b2f680563f91156c
Shreya <Shreyaroxx777@gmail.com>
2020-06-14 19:51:44 +0530

    updated

---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("D:\\My Work\\Website\\DCV 2\\DCV\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("D:\\My Work\\Website\\DCV 2\\DCV\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("D:\\My Work\\Website\\DCV 2\\DCV\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
